d78b57e858ce16b47e4a82a03dc7340ea9dad806
sandeep-19 <sandeep.rqs@gmail.com>
2022-10-30 23:19:52 +0530

    reverting delete node modules

--- public/client/landing.js
+++ public/client/landing.js
@@ -18,30 +18,29 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 
 //hero ani
 gsap.registerPlugin(ScrollTrigger);
-window.onload = function () {
-  gsap
-    .timeline()
-    .fromTo(
-      "#title",
-      {
-        scale: 0,
-        opacity: 0,
-      },
-      { scale: 1, duration: 3, opacity: 1, y: "30vh" }
-    )
-    .fromTo(
-      "#hero-dep",
-      { scale: 0, opacity: 0 },
-      { scale: 1, duration: 3, opacity: 1, y: "30vh" },
-      "-=1"
-    )
-    .to("#boom", { duration: 3, x: "60vw", ease: "back(2)" })
-    .to("#boom", { y: "-10vh", duration: 2 })
-    .to("#boom", { x: "30vw", duration: 3 }, "-=2")
-    .to("#hinge", { duration: 1, rotation: 50 }, "-=4")
-    .to("#hinge", { y: "200vh", duration: 1 }, "-=3")
-    .to("#hinge", { display: "none" }, "-=2");
-};
+gsap
+  .timeline()
+  .fromTo(
+    "#title",
+    {
+      scale: 0,
+      opacity: 0,
+    },
+    { scale: 1, duration: 3, opacity: 1, y: "30vh" }
+  )
+  .fromTo(
+    "#hero-dep",
+    { scale: 0, opacity: 0 },
+    { scale: 1, duration: 3, opacity: 1, y: "30vh" },
+    "-=1"
+  )
+  .to("#boom", { duration: 3, x: "60vw", ease: "back(2)" })
+  .to("#boom", { y: "-10vh", duration: 2 })
+  .to("#boom", { x: "30vw", duration: 3 }, "-=2")
+  .to("#hinge", { duration: 1, rotation: 50 }, "-=4")
+  .to("#hinge", { y: "200vh", duration: 1 }, "-=3")
+  .to("#hinge", { display: "none" }, "-=2");
+
 //about ani
 //1 about
 gsap
