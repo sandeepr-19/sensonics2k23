5dbc04c29b222419056ff3e63a3b4302fb7885c0
sandeep-19 <sandeep.rqs@gmail.com>
2022-11-13 10:08:53 +0530

    regi end commit1

--- index.js
+++ index.js
@@ -37,179 +37,179 @@ app.get("/event6", (req, res) => {
   res.render("event6.pug");
 });
 
-app.post("/event1_d", enc, (req, res) => {
-  let tname = req.body.tname;
-  let name1 = req.body.name1;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  let title = req.body.title;
-  let abstract = req.body.abstract;
-  let name2 = req.body.name2;
-  let rollno2 = req.body.rollno2;
-  let name3 = req.body.name3;
-  let rollno3 = req.body.rollno3;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event1_d", enc, (req, res) => {
+//   let tname = req.body.tname;
+//   let name1 = req.body.name1;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   let title = req.body.title;
+//   let abstract = req.body.abstract;
+//   let name2 = req.body.name2;
+//   let rollno2 = req.body.rollno2;
+//   let name3 = req.body.name3;
+//   let rollno3 = req.body.rollno3;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      team_name: tname,
-      name1: name1,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-      title: title,
-      abstract: abstract,
-      name2: name2,
-      rollno2: rollno2,
-      name3: name3,
-      rollno3: rollno3,
-    };
-    const result = await dbo.collection("event1").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       team_name: tname,
+//       name1: name1,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//       title: title,
+//       abstract: abstract,
+//       name2: name2,
+//       rollno2: rollno2,
+//       name3: name3,
+//       rollno3: rollno3,
+//     };
+//     const result = await dbo.collection("event1").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
-app.post("/event2_d", enc, (req, res) => {
-  let tname = req.body.tname;
-  let name1 = req.body.name1;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  let title = req.body.title;
-  let abstract = req.body.abstract;
-  let name2 = req.body.name2;
-  let rollno2 = req.body.rollno2;
-  let name3 = req.body.name3;
-  let rollno3 = req.body.rollno3;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event2_d", enc, (req, res) => {
+//   let tname = req.body.tname;
+//   let name1 = req.body.name1;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   let title = req.body.title;
+//   let abstract = req.body.abstract;
+//   let name2 = req.body.name2;
+//   let rollno2 = req.body.rollno2;
+//   let name3 = req.body.name3;
+//   let rollno3 = req.body.rollno3;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      team_name: tname,
-      name1: name1,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-      title: title,
-      abstract: abstract,
-      name2: name2,
-      rollno2: rollno2,
-      name3: name3,
-      rollno3: rollno3,
-    };
-    const result = await dbo.collection("event2").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       team_name: tname,
+//       name1: name1,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//       title: title,
+//       abstract: abstract,
+//       name2: name2,
+//       rollno2: rollno2,
+//       name3: name3,
+//       rollno3: rollno3,
+//     };
+//     const result = await dbo.collection("event2").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
-app.post("/event3_d", enc, (req, res) => {
-  let name = req.body.name;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event3_d", enc, (req, res) => {
+//   let name = req.body.name;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      name: name,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-    };
-    const result = await dbo.collection("event3").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       name: name,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//     };
+//     const result = await dbo.collection("event3").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
-app.post("/event4_d", enc, (req, res) => {
-  let name = req.body.name;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event4_d", enc, (req, res) => {
+//   let name = req.body.name;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      name: name,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-    };
-    const result = await dbo.collection("event4").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       name: name,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//     };
+//     const result = await dbo.collection("event4").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
-app.post("/event5_d", enc, (req, res) => {
-  let tname = req.body.tname;
-  let name1 = req.body.name1;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  let name2 = req.body.name2;
-  let rollno2 = req.body.rollno2;
-  let name3 = req.body.name3;
-  let rollno3 = req.body.rollno3;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event5_d", enc, (req, res) => {
+//   let tname = req.body.tname;
+//   let name1 = req.body.name1;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   let name2 = req.body.name2;
+//   let rollno2 = req.body.rollno2;
+//   let name3 = req.body.name3;
+//   let rollno3 = req.body.rollno3;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      team_name: tname,
-      name1: name1,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-      name2: name2,
-      rollno2: rollno2,
-      name3: name3,
-      rollno3: rollno3,
-    };
-    const result = await dbo.collection("event5").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       team_name: tname,
+//       name1: name1,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//       name2: name2,
+//       rollno2: rollno2,
+//       name3: name3,
+//       rollno3: rollno3,
+//     };
+//     const result = await dbo.collection("event5").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
-app.post("/event6_d", enc, (req, res) => {
-  let tname = req.body.tname;
-  let name1 = req.body.name1;
-  let rollno = req.body.rollno;
-  let email = req.body.email;
-  let mobileno = req.body.mobileno;
-  let name2 = req.body.name2;
-  let rollno2 = req.body.rollno2;
-  let name3 = req.body.name3;
-  let rollno3 = req.body.rollno3;
-  MongoClient.connect(db_url, async (err, db) => {
-    if (err) throw err;
-    const dbo = db.db("event-details");
+// app.post("/event6_d", enc, (req, res) => {
+//   let tname = req.body.tname;
+//   let name1 = req.body.name1;
+//   let rollno = req.body.rollno;
+//   let email = req.body.email;
+//   let mobileno = req.body.mobileno;
+//   let name2 = req.body.name2;
+//   let rollno2 = req.body.rollno2;
+//   let name3 = req.body.name3;
+//   let rollno3 = req.body.rollno3;
+//   MongoClient.connect(db_url, async (err, db) => {
+//     if (err) throw err;
+//     const dbo = db.db("event-details");
 
-    const doc = {
-      team_name: tname,
-      name1: name1,
-      rollno: rollno,
-      email: email,
-      mobileno: mobileno,
-      name2: name2,
-      rollno2: rollno2,
-      name3: name3,
-      rollno3: rollno3,
-    };
-    const result = await dbo.collection("event6").insertOne(doc);
-    db.close();
-  });
-  res.render("landing.pug");
-});
+//     const doc = {
+//       team_name: tname,
+//       name1: name1,
+//       rollno: rollno,
+//       email: email,
+//       mobileno: mobileno,
+//       name2: name2,
+//       rollno2: rollno2,
+//       name3: name3,
+//       rollno3: rollno3,
+//     };
+//     const result = await dbo.collection("event6").insertOne(doc);
+//     db.close();
+//   });
+//   res.render("landing.pug");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
